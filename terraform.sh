447da4631d81dfdf68d07df806b18cd9f2df6a9c
Hristo Dragolov <root@elenakravaykova_centos7.bgdev.mansion.gi>
2016-11-10 15:03:02 +0000

    Variables names fixes

--- terraform.sh
+++ terraform.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # Installer script for terraform
-TMP_DIR='/tmp/terraform'
-ZIP_FILE='terraform.zip'
-VERSION='0.7.10'
-SOURCE="https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip"
+tmp_dir='/tmp/terraform'
+zip_file='terraform.zip'
+version='0.7.10'
+source="https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip"
 
 echo -e "\n### Pulling terraform pkg zip ###\n"
-mkdir "${TMP_DIR}" && cd "${TMP_DIR}"
-curl -s -o "${ZIP_FILE}" "${SOURCE}"
+mkdir "${tmp_dir}" && cd "${tmp_dir}"
+curl -s -o "${zip_file}" "${source}"
 
 echo -e "\n### Extracting archive ###\n"
-unzip "${ZIP_FILE}"
+unzip "${zip_file}"
 
 echo -e "\n### Moving binary to /usr/loca/bin ###\n"
 mv terraform /usr/local/bin
 
 echo -e "\n### Removing temporary files ###"
-rm -rf "${TMP_DIR}"
+rm -rf "${tmp_dir}"
